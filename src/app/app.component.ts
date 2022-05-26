dde67ce8e483f71ec39dd32607a90773f22d0724
BogdanMoskvitin <moskvitin.bg@gmail.com>
2022-05-26 17:42:33 +0300

    add data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,71 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+  breakfast = [{ id: 1, name: 'Бутерброд', value: 2 }, { id: 2, name: 'Какао', value: 1 },]
+  lunch = [{ id: 3, name: 'Хлеб', value: 2 }, { id: 4, name: 'Суп', value: 0 },]
+  dinner = [{ id: 5, name: 'Булочка', value: 2 }, { id: 6, name: 'Чай', value: 1 },]
+
+  view = {
+    breakfast: false,
+    lunch: false,
+    dinner: false,
+  }
+
+  select = []
+
+  sum = 0
+
+  popup = false
+
+  viewBreakfast() {
+    this.view = {
+      breakfast: true,
+      lunch: false,
+      dinner: false,
+    }
+  }
+
+  viewLunch() {
+    this.view = {
+      breakfast: false,
+      lunch: true,
+      dinner: false,
+    }
+  }
+
+  viewDinner() {
+    this.view = {
+      breakfast: false,
+      lunch: false,
+      dinner: true,
+    }
+  }
+
+  addEl(el) {
+    this.select.push(el)
+  }
+
+  deleteEl(el) {
+    this.select.forEach(res => {
+      if(res.id == el.id){
+        let index = this.select.indexOf(res)
+        this.select.splice(index, 1)
+      }
+    })
+  }
+
+  calc() {
+    this.select.forEach(res => {
+      this.sum += res.value
+    })
+    this.popup = true
+  }
+
+  back() {
+    this.popup = false
+  }
+
+  clean() {
+    this.select = []
+  }
 }
