9c5978f3ab7b4e76df85cb56ea792df061fa10a8
BogdanMoskvitin <moskvitin.bg@gmail.com>
2022-06-01 14:24:43 +0300

    calc sugar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { FormControl } from '@angular/forms';
+import { FormGroup, Validators } from '@angular/forms';
+import { Food } from './food.model';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
-import { Food } from './food.model';
 import { mockFood } from './food.mock';
-import { FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent implements OnInit {
+  select = []
+  foodControl = new FormControl()
+  filteredFood: Observable<Food[]>
+  listFood: Food[] = mockFood
+  sum = 0
+  round = 0
+  popup = false
+  question: string
+  food: Food;
+  needSugar = 0;
 
-  myControl = new FormControl();
-  filteredOptions: Observable<Food[]>;
-
-  options: Food[] = mockFood;
-
-  select = [];
-  sum = 0;
-  round = 0;
-  popup = false;
-  question: string;
-  el: Food;
-
-  myForm : FormGroup = new FormGroup({
+  formFood : FormGroup = new FormGroup({
     "count": new FormControl("", [Validators.required, Validators.pattern("^[0-9]*$")]),
   })
 
+  sugarForm : FormGroup = new FormGroup({
+    "sugar": new FormControl("", [Validators.required, Validators.pattern("^[0-9]*$")]),
+  })
+
   ngOnInit() {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
+    this.filteredFood = this.foodControl.valueChanges.pipe(
       startWith(''),
       map(value => (typeof value === 'string' ? value : value.name)),
-      map(name => (name ? this._filter(name) : this.options.slice())),
+      map(name => (name ? this._filter(name) : this.listFood.slice())),
     );
   }
 
@@ -44,27 +47,12 @@ export class AppComponent implements OnInit {
   private _filter(name: string): Food[] {
     const filterValue = name.toLowerCase();
 
-    return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
+    return this.listFood.filter(option => option.name.toLowerCase().includes(filterValue));
   }
 
-  addEl(el) {
-    this.el = el
-    this.question = 'Сколько ' + el.question + ' ты съела?'
-  }
-
-  submit() {
-    let arr = {
-      el: this.el,
-      count: this.myForm.value.count,
-    }
-    this.select.push(arr)
-    this.myControl.setValue('')
-    this.myForm.reset()
-  }
-
-  deleteEl(el) {
+  deleteFood(food) {
     for(let i = 0; i < this.select.length; i++) {
-      if(this.select[i].id == el.id) {
+      if(this.select[i].id == food.id) {
         let index = this.select.indexOf(this.select[i])
         this.select.splice(index, 1)
         break
@@ -72,21 +60,48 @@ export class AppComponent implements OnInit {
     }
   }
 
-  calc() {
+  addFood(food) {
+    this.food = food
+    this.question = 'Сколько ' + food.question + ' ты съела?'
+  }
+
+  sendFormFood() {
+    let arr = {
+      food: this.food,
+      count: this.formFood.value.count,
+    }
+    this.select.push(arr)
+    this.foodControl.setValue('')
+    this.formFood.reset()
+  }
+
+  calcFood() {
     this.select.forEach(res => {
-      this.sum += (res.count * res.el.min)
+      this.sum += (res.count * res.food.min)
     })
     this.round = Math.round(this.sum)
     this.sum = +this.sum.toFixed(3)
     this.popup = true
   }
 
+  clear() {
+    this.select = []
+  }
+
+  sendSugarForm() {
+    if(this.sugarForm.value.sugar > 7) {
+      this.needSugar = this.sugarForm.value.sugar - 7;
+    } else if (this.sugarForm.value.sugar < 7) {
+      this.needSugar = -(7 - this.sugarForm.value.sugar)
+    } else {
+      this.needSugar = 0
+    }
+    this.needSugar += this.round
+  }
+
+
   back() {
     this.popup = false
     this.sum = 0;
-  }
-
-  clean() {
-    this.select = []
   }
 }
